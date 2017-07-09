77ee5f8fbd74fb3892be6c84fb859467fc86a63e
Adam Hakowski <a.hakowski@insync365.pl>
2017-07-09 13:30:15 +0200

    moved web app

--- TaskListApp/src/app/tasks/tasks.module.ts
+++ TaskListApp/src/app/tasks/tasks.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TaskComponent } from './task/task.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [TaskComponent]
 })
 export class TasksModule { }
