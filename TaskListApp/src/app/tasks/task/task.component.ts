5ce7138da11a102ffe3c8a481e6bb69016286fe4
Adam Hakowski <a.hakowski@insync365.pl>
2017-07-11 22:07:50 +0200

    dumpy component task

--- TaskListApp/src/app/tasks/task/task.component.ts
+++ TaskListApp/src/app/tasks/task/task.component.ts
@@ -1,10 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-task',
   templateUrl: './task.component.html'
 })
 export class TaskComponent implements OnInit {
+
+  @Input() task;
 
   constructor() { }
 
